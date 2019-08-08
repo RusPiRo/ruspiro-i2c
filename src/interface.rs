6a52fa0043c0d3eb6dce4cdf90162b4360e6249f
Borrmann <43264484+2ndTaleStudio@users.noreply.github.com>
2019-08-08 14:01:44 +0200

    i2c basic implementation

--- src/interface.rs
+++ src/interface.rs
@@ -12,32 +12,30 @@
 extern crate alloc;
 use alloc::{vec, vec::Vec};
 
-use ruspiro_register::define_registers;
+use ruspiro_register::{define_registers, RegisterFieldValue};
 use ruspiro_timer as timer;
 use ruspiro_gpio::GPIO;
 use ruspiro_console::*;
 
-use crate::I2C;
-
 #[cfg(feature="ruspiro_pi3")]
 const PERIPHERAL_BASE: u32 = 0x3F00_0000;
 
-const I2C_BASE: u32 = PERIPHERAL_BASE + 0x0080_4000;
-const I2C_MAX_BYTES: u16 = 16;
-const I2C_DEFAULT_WAIT: u32 = 2000;
+const I2C_BASE: u32 = PERIPHERAL_BASE + 0x0080_4000; // I²C peripheral register base address
+const I2C_MAX_BYTES: u16 = 16; // max FiFo size of the I²C peripheral
+const I2C_DEFAULT_WAIT: u32 = 2000; // max cycles to wait for a device to acknowledge a request
 
-type I2cResult<T> = Result<T, &'static str>;
+use crate::I2cResult;
 
 pub(crate) fn initialize(core_speed: u32, fast_mode: bool) -> I2cResult<()> {
     // when I2C is about to be initialized reserve GPIO Pins 2 and 3
-    // as the I2C bus pins
-    GPIO.take_for(|gpio| gpio.get_pin(2))
-        .map(|pin| pin.to_alt_f0())
-        .and_then(|_| GPIO.take_for(|gpio| gpio.get_pin(3)))
-        .map(|pin| pin.to_alt_f0())
-        .and_then(|_| {
-            // both pin's configured
-            // setup the I2C speed
+    // as the I2C bus pins with alt function 0
+    GPIO.take_for(|gpio| {
+        gpio.get_pin(2)
+            .and_then(|pin| { pin.to_alt_f0(); Ok(()) })
+            .and_then(|_| gpio.get_pin(3))
+            .and_then(|pin| { pin.to_alt_f0(); Ok(()) })
+    }).and_then(|_| {
+            // both pin's configured, now setup the I2C speed and we are done
             let clock_divisor = if fast_mode {
                 core_speed / 400_000
             } else {
@@ -49,16 +47,43 @@ pub(crate) fn initialize(core_speed: u32, fast_mode: bool) -> I2cResult<()> {
         })
 }
 
+// helper macros to re-use register field value specifications for specific actions
+macro_rules! status_clear {
+    () => {
+        RegisterFieldValue::<u32>::new(I2C_REG_S::CLK_TIMEOUT, 1) |
+        RegisterFieldValue::<u32>::new(I2C_REG_S::ACK_ERROR, 1) |
+        RegisterFieldValue::<u32>::new(I2C_REG_S::TRANS_DONE, 1)
+    };
+}
+
+macro_rules! control_startread {
+    () => {
+        RegisterFieldValue::<u32>::new(I2C_REG_C::ENABLE, 1) |
+        RegisterFieldValue::<u32>::new(I2C_REG_C::STARTTRANS, 1) |
+        RegisterFieldValue::<u32>::new(I2C_REG_C::CLEAR, 1) |
+        RegisterFieldValue::<u32>::new(I2C_REG_C::READWRITE, 1)
+    };
+}
+
+macro_rules! control_startwrite {
+    () => {
+        RegisterFieldValue::<u32>::new(I2C_REG_C::ENABLE, 1) |
+        RegisterFieldValue::<u32>::new(I2C_REG_C::STARTTRANS, 1) |
+        RegisterFieldValue::<u32>::new(I2C_REG_C::READWRITE, 0)
+    };
+}
+
 /// Scan for I2C devices currently connected to the I2C bus. The scan will just try to get an acknowledge message
 /// from any slave address between 0x00 and 0x7F. If a device is connected this call succeeds and the corresponding
 /// address is written to the console
 pub(crate) fn scan_devices() -> Vec<u8> {
     let mut r: Vec<u8> = vec![];
+
     for addr in 0x00..0x80 {
         I2C_REG_A::Register.set(addr);
         I2C_REG_DLEN::Register.set(1);
-        I2C_REG_S::Register.set(StatusFlag::Clear as u32);
-        I2C_REG_C::Register.set(ControlFlag::StartRead as u32);
+        I2C_REG_S::Register.write_value(status_clear!());
+        I2C_REG_C::Register.write_value(control_startread!());
 
         if wait_i2c_done(100).is_ok() {
             r.push(addr as u8);
@@ -71,30 +96,30 @@ pub(crate) fn scan_devices() -> Vec<u8> {
 pub(crate) fn check_device(addr: u8) -> I2cResult<()> {
     I2C_REG_A::Register.set(addr as u32);
     I2C_REG_DLEN::Register.set(1);
-    I2C_REG_S::Register.set(StatusFlag::Clear as u32);
-    I2C_REG_C::Register.set(ControlFlag::StartRead as u32);
+    I2C_REG_S::Register.write_value(status_clear!());
+    I2C_REG_C::Register.write_value(control_startread!());
 
     wait_i2c_done(100)
 }
 
 pub(crate) fn read_reg_u8(addr: u32, reg: u32) -> I2cResult<u8> {
-    // aquire atmic access to the I2C peripheral, this is necessary as the device
-    // is accessible outside the I2C peripheral.
-    I2C.take_for(|_| {
-        write_register(addr, reg)
-            .and_then(|_| {
-                I2C_REG_DLEN::Register.set(1);
-                I2C_REG_S::Register.set(StatusFlag::Clear as u32);
-                I2C_REG_C::Register.set(ControlFlag::StartRead as u32);
-                wait_i2c_done(I2C_DEFAULT_WAIT)
-                    .map_err(|e| {
-                        error!("read u8 addr:{:x} / reg:{:x} - {} - status {:x}", addr, reg, e, I2C_REG_S::Register.get());
-                        e
-                    })
-            }).and_then(|_| {
-                Ok(read_fifo(1)[0])
-            })
-    })
+    // reading I²C device regiser data means:
+    // 1. write the register address to the device and wait for acknowledge
+    // 2. read from the device and wait for acknowledge
+    // 3. data available in the fifo
+    write_register(addr, reg)
+        .and_then(|_| {
+            I2C_REG_DLEN::Register.set(1);
+            I2C_REG_S::Register.write_value(status_clear!());
+            I2C_REG_C::Register.write_value(control_startread!());
+            wait_i2c_done(I2C_DEFAULT_WAIT)
+                .map_err(|e| {
+                    error!("read u8 addr:{:x} / reg:{:x} - {} - status {:x}", addr, reg, e, I2C_REG_S::Register.get());
+                    e
+                })
+        }).and_then(|_| {
+            Ok(read_fifo(1)[0])
+        })
 }
 
 pub(crate) fn read_reg_u16(addr: u32, reg: u32) -> I2cResult<u16> {
@@ -113,78 +138,71 @@ pub(crate) fn read_reg_bits(addr: u32, reg: u32, offset: u16, bits: u16) -> I2cR
 }
 
 pub(crate) fn read_reg_data(addr: u32, reg: u32, count: u16) -> I2cResult<Vec<u8>> {
-    // aquire atmic access to the I2C peripheral, this is necessary as the device
-    // is accessible outside the I2C peripheral.
-    I2C.take_for(|_| {
-        write_register(addr, reg)
-            .and_then(|_| {                    
-                I2C_REG_DLEN::Register.set(count as u32);
-                I2C_REG_S::Register.set(StatusFlag::Clear as u32);
-                I2C_REG_C::Register.set(ControlFlag::StartRead as u32);
-                wait_i2c_done(I2C_DEFAULT_WAIT)
-                    .map_err(|e| {
-                        error!("read data from addr:{:x} / reg:{:x} - {}", addr, reg, e);
-                        e
-                    })
-            }).and_then(|_| {
-                let mut data: Vec<u8> = Vec::with_capacity(count as usize);
-                let chunks = count / I2C_MAX_BYTES;
-                let mut remainder = count;
-                for _ in 0..chunks+1 {
-                    let size = if remainder > I2C_MAX_BYTES { I2C_MAX_BYTES } else { remainder };
-                    data.extend(read_fifo(size as u8).into_iter());
-                    remainder -= I2C_MAX_BYTES;
-                }
-                Ok(data)
-            })
-    })
+    // reading I²C device regiser data means:
+    // 1. write the register address to the device and wait for acknowledge
+    // 2. read from the device and wait for acknowledge
+    // 3. data available in the fifo
+    write_register(addr, reg)
+        .and_then(|_| {                    
+            I2C_REG_DLEN::Register.set(count as u32);
+            I2C_REG_S::Register.write_value(status_clear!());
+            I2C_REG_C::Register.write_value(control_startread!());
+            wait_i2c_done(I2C_DEFAULT_WAIT)
+                .map_err(|e| {
+                    error!("read data from addr:{:x} / reg:{:x} - {}", addr, reg, e);
+                    e
+                })
+        }).and_then(|_| {
+            let mut data: Vec<u8> = Vec::with_capacity(count as usize);
+            let chunks = count / I2C_MAX_BYTES;
+            let mut remainder = count;
+            for _ in 0..chunks+1 {
+                let size = if remainder > I2C_MAX_BYTES { I2C_MAX_BYTES } else { remainder };
+                data.extend(read_fifo(size as u8).into_iter());
+                remainder -= I2C_MAX_BYTES;
+            }
+            Ok(data)
+        })
 }
 
 pub(crate) fn write_raw_u8(addr: u32, data: u8) -> I2cResult<()> {
-    // aquire atmic access to the I2C peripheral, this is necessary as the device
-    // is accessible outside the I2C peripheral.
-    I2C.take_for(|_| {
-        // clear status flags
-        I2C_REG_S::Register.set(StatusFlag::Clear as u32);
-        // clear FiFo data in case FiFo data has remained from previous calls
-        I2C_REG_C::Register.set(ControlFlag::Clear as u32);
-        // set the slave address we would like to send data to and the register id
-        I2C_REG_A::Register.set(addr);
-        I2C_REG_DLEN::Register.set(1);
-        I2C_REG_FIFO::Register.set(data as u32);
-        // transmit the data
-        I2C_REG_C::Register.set(ControlFlag::StartWrite as u32);
+    
+    // clear status flags
+    I2C_REG_S::Register.write_value(status_clear!());
+    // clear FiFo data in case FiFo data has remained from previous calls
+    I2C_REG_C::Register.write(I2C_REG_C::CLEAR, 1);
+    // set the slave address we would like to send data to and the register id
+    I2C_REG_A::Register.set(addr);
+    I2C_REG_DLEN::Register.set(1);
+    I2C_REG_FIFO::Register.set(data as u32);
+    // transmit the data
+    I2C_REG_C::Register.write_value(control_startwrite!());
 
-        wait_i2c_done(I2C_DEFAULT_WAIT)
-            .map_err(|e| {
-                error!(" write raw to addr:{:x} - {}", addr, e);
-                e
-            })
-    })
+    wait_i2c_done(I2C_DEFAULT_WAIT)
+        .map_err(|e| {
+            error!(" write raw to addr:{:x} - {}", addr, e);
+            e
+        })
 }
 
 pub(crate) fn write_reg_u8(addr: u32, reg: u32, data: u8) -> I2cResult<()> {
-    // aquire atmic access to the I2C peripheral, this is necessary as the device
-    // is accessible outside the I2C peripheral.
-    I2C.take_for(|_| {
-        // clear status flags
-        I2C_REG_S::Register.set(StatusFlag::Clear as u32);
-        // clear FiFo data in case FiFo data has remained from previous calls
-        I2C_REG_C::Register.set(ControlFlag::Clear as u32);
-        // set the slave address we would like to send data to and the register id
-        I2C_REG_A::Register.set(addr);
-        I2C_REG_DLEN::Register.set(2);
-        I2C_REG_FIFO::Register.set(reg);
-        I2C_REG_FIFO::Register.set(data as u32);
-        // transmit the data
-        I2C_REG_C::Register.set(ControlFlag::StartWrite as u32);
+    // clear status flags
+    I2C_REG_S::Register.write_value(status_clear!());
+    // clear FiFo data in case FiFo data has remained from previous calls
+    I2C_REG_C::Register.write(I2C_REG_C::CLEAR, 1);
+    // set the slave address we would like to send data to and the register id
+    I2C_REG_A::Register.set(addr);
+    I2C_REG_DLEN::Register.set(2);
+    I2C_REG_FIFO::Register.set(reg);
+    I2C_REG_FIFO::Register.set(data as u32);
+    // transmit the data
+    I2C_REG_C::Register.write_value(control_startwrite!());
 
-        wait_i2c_done(I2C_DEFAULT_WAIT)
-            .map_err(|e| {
-                error!("write u8 to addr:{:x} / reg:{:x} - {}", addr, reg, e);
-                e
-            })
-    })
+    wait_i2c_done(I2C_DEFAULT_WAIT)
+        .map_err(|e| {
+            error!("write u8 to addr:{:x} / reg:{:x} - {}", addr, reg, e);
+            e
+        })
 }
 
 pub(crate) fn write_reg_u16(addr: u32, reg: u32, data: u16) -> I2cResult<()> {
@@ -202,38 +220,35 @@ pub(crate) fn write_reg_bits(addr: u32, reg: u32, offset: u16, bits: u16, value:
 }
 
 pub(crate) fn write_reg_data(addr: u32, reg: u32, data: &[u8]) -> I2cResult<()> {
-    // aquire atmic access to the I2C peripheral, this is necessary as the device
-    // is accessible outside the I2C peripheral.
-    I2C.take_for(|_| {
-        let mut data_len = data.len() as u32;
-        // clear status flags
-        I2C_REG_S::Register.set(StatusFlag::Clear as u32);
-        // clear FiFo data in case FiFo data has remained from previous calls
-        I2C_REG_C::Register.set(ControlFlag::Clear as u32);
-        // set the slave address we would like to send data to and the register id
-        I2C_REG_A::Register.set(addr);
-        I2C_REG_DLEN::Register.set(data_len+1);
-        I2C_REG_FIFO::Register.set(reg);
-        // transmit the data
-        I2C_REG_C::Register.set(ControlFlag::StartWrite as u32);
-        let chunks = data_len/I2C_MAX_BYTES as u32;
-        for chunk in 0..chunks+1 {
-            let idx: usize = (chunk*data_len) as usize;
-            let len: usize = if data_len > I2C_MAX_BYTES as u32 {
-                I2C_MAX_BYTES as usize
-            } else {
-                data_len as usize
-            };
-            write_fifo(&data[idx..len]);
-            data_len -= I2C_MAX_BYTES as u32;
-        }
 
-        wait_i2c_done(I2C_DEFAULT_WAIT)
-            .map_err(|e| {
-                error!("write data to addr:{:x} / reg:{:x} - {}", addr, reg, e);
-                e
-            })
-    })
+    let mut data_len = data.len() as u32;
+    // clear status flags
+    I2C_REG_S::Register.write_value(status_clear!());
+    // clear FiFo data in case FiFo data has remained from previous calls
+    I2C_REG_C::Register.write(I2C_REG_C::CLEAR, 1);
+    // set the slave address we would like to send data to and the register id
+    I2C_REG_A::Register.set(addr);
+    I2C_REG_DLEN::Register.set(data_len+1);
+    I2C_REG_FIFO::Register.set(reg);
+    // transmit the data
+    I2C_REG_C::Register.write_value(control_startwrite!());
+    let chunks = data_len/I2C_MAX_BYTES as u32;
+    for chunk in 0..chunks+1 {
+        let idx: usize = (chunk*data_len) as usize;
+        let len: usize = if data_len > I2C_MAX_BYTES as u32 {
+            I2C_MAX_BYTES as usize
+        } else {
+            data_len as usize
+        };
+        write_fifo(&data[idx..len]);
+        data_len -= I2C_MAX_BYTES as u32;
+    }
+
+    wait_i2c_done(I2C_DEFAULT_WAIT)
+        .map_err(|e| {
+            error!("write data to addr:{:x} / reg:{:x} - {}", addr, reg, e);
+            e
+        })
 }
 
 
@@ -260,8 +275,8 @@ fn write_register(addr: u32, reg: u32) -> I2cResult<()> {
     I2C_REG_DLEN::Register.set(1);
     I2C_REG_FIFO::Register.set(reg);
     // transmit the data
-    I2C_REG_S::Register.set(StatusFlag::Clear as u32);
-    I2C_REG_C::Register.set(ControlFlag::StartWrite as u32);
+    I2C_REG_S::Register.write_value(status_clear!());
+    I2C_REG_C::Register.write_value(control_startwrite!());
 
     wait_i2c_done(I2C_DEFAULT_WAIT)
         .map_err(|e| {
@@ -291,60 +306,32 @@ fn write_fifo(data: &[u8]) {
 // I2C register definitions
 define_registers! [
     I2C_REG_C:    ReadWrite<u32> @ I2C_BASE + 0x00 => [ // control register
-        ENABLE     OFFSET(15),
-        IRQ_RX     OFFSET(10),
-        IRQ_TX     OFFSET(9),
-        IRQ_DONE   OFFSET(8),
-        STARTTRANS OFFSET(7),
-        CLEAR      OFFSET(4),
-        READ       OFFSET(1),
-        WRITE      OFFSET(0)
-
+        ENABLE     OFFSET(15),  // 1 I²C bus enabled
+        IRQ_RX     OFFSET(10),  // 1 Receive interrupt enabled
+        IRQ_TX     OFFSET(9),   // 1 Transmit interrupt enabled
+        IRQ_DONE   OFFSET(8),   // 1 Done interrupt enabled
+        STARTTRANS OFFSET(7),   // 1 Start transfer
+        CLEAR      OFFSET(4),   // 1 clear fifo buffer
+        READWRITE  OFFSET(0)    // 1 Read / 0 Write operation
     ],
     I2C_REG_S:    ReadWrite<u32> @ I2C_BASE + 0x04 => [ // status register
-        CLK_TIMEOUT  OFFSET(9),
-        ACK_ERROR    OFFSET(8),
-        RX_FULL      OFFSET(7),
-        TX_EMPTY     OFFSET(6),
-        RX_DATA      OFFSET(5),
-        TX_DATA      OFFSET(4),
-        RX_NEEDREAD  OFFSET(3),
-        TX_NEEDWRITE OFFSET(2),
-        TRANS_DONE   OFFSET(1),
-        TRANS_ACTIVE OFFSET(0)
+        CLK_TIMEOUT  OFFSET(9), // 1 Slave has held the SCL signal longer than allowed high
+        ACK_ERROR    OFFSET(8), // 1 Slave address acknowledge error
+        RX_FULL      OFFSET(7), // 1 FIFO is full
+        TX_EMPTY     OFFSET(6), // 1 FIFO is empty
+        RX_DATA      OFFSET(5), // 1 FIFO contains at least one byte
+        TX_DATA      OFFSET(4), // 1 FIFO can accept data
+        RX_NEEDREAD  OFFSET(3), // 1 FIFO is full and needs reading from the FIFO
+        TX_NEEDWRITE OFFSET(2), // 1 FIFO is less than full and needs writing to the FIFO
+        TRANS_DONE   OFFSET(1), // 1 if transfer is complete
+        TRANS_ACTIVE OFFSET(0)  // 1 if transfer is active
     ],
     I2C_REG_DLEN: ReadWrite<u32> @ I2C_BASE + 0x08 => [   // data len register
         DATA OFFSET(0) BITS(16)
     ],
     I2C_REG_A:    ReadWrite<u32> @ I2C_BASE + 0x0C, // slave address register
-    I2C_REG_FIFO: ReadWrite<u32> @ I2C_BASE + 0x10,
-    I2C_REG_CDIV: ReadWrite<u32> @ I2C_BASE + 0x14,
-    I2C_REG_DEL:  ReadWrite<u32> @ I2C_BASE + 0x18,
-    I2C_REG_CLKT: ReadWrite<u32> @ I2C_BASE + 0x1C
+    I2C_REG_FIFO: ReadWrite<u32> @ I2C_BASE + 0x10, // FiFo data register
+    I2C_REG_CDIV: ReadWrite<u32> @ I2C_BASE + 0x14, // clock divisor
+    I2C_REG_DEL:  ReadWrite<u32> @ I2C_BASE + 0x18, // data delay
+    I2C_REG_CLKT: ReadWrite<u32> @ I2C_BASE + 0x1C  // clock stretch timeout
 ];
-
-#[repr(u32)]
-enum ControlFlag {
-    Enable =     1 << 15,
-    StartTrans = 1 << 7,
-    Clear =      1 << 4,
-    Read  =      1 << 1,
-    Write =      1 << 0,
-    StartRead  = (1 << 15) | (1 << 7) | (1 << 4) | (1 << 0),
-    StartWrite = (1 << 15) | (1 << 7) | (0 << 0),
-}
-
-#[repr(u32)]
-enum StatusFlag {
-    ClckTimeout = 	1 << 9,	// 1 Slave has held the SCL signal longer than allowed high
-    AckError    =	1 << 8,	// 1 Slave address acknowledge error
-    RxFull      =	1 << 7,	// 1 FIFO is full
-    TxEmpty     =	1 << 6,	// 1 FIFO is empty
-    RxData      = 	1 << 5,	// 1 FIFO contains at least one byte
-    TxData      =	1 << 4,	// 1 FIFO can accept data
-    RxNeedRead  =	1 << 3,	// 1 FIFO is full and needs reading from the FIFO
-    TxNeedWrite =	1 << 2,	// 1 FIFO is less than full and needs writing to the FIFO
-    TransDone   =   1 << 1,	// 1 if transfer is complete
-    TransActive = 	1 << 0,	// 1 if transfer is active
-    Clear       = (1 << 9) | (1 << 8) | (1 << 1),
-}
--- src/lib.rs
+++ src/lib.rs
@@ -11,24 +11,138 @@
 //! 
 //! 
 //! 
-
+extern crate alloc;
+use alloc::vec::Vec;
 use ruspiro_singleton::Singleton;
 use ruspiro_console::*;
 
 mod interface;
 
 /// static singleton accessor for the I²C bus peripheral
-pub static I2C: Singleton<I2c> = Singleton::new(I2c::new());
+pub static I2C: Singleton<I2cImpl> = Singleton::new(I2cImpl::new());
 
 /// I²C peripheral representation
-pub struct I2c {
+pub struct I2cImpl {
     initialized: bool,
 }
 
-impl I2c {
+/// I²C device representation
+pub struct I2cDevice {
+    addr: u32,
+}
+
+type I2cResult<T> = Result<T, &'static str>;
+
+impl I2cImpl {
+    /// create a new instance of the I2c implementation. This will only be used to
+    /// prepare the static singleton I²C accessor.
     pub(crate) const fn new() -> Self {
-        I2c {
+        I2cImpl {
             initialized: false,
+        }
+    }
+
+    /// Initialize the I²C bus for further usage. This will require the GPIO pins 2 and 3 to be available for usage.
+    /// If they have been already occupied before this initialization is called an error will be returned.
+    pub fn initialize(&mut self, core_speed: u32, fast_mode: bool) -> I2cResult<()>{
+        if !self.initialized {
+            interface::initialize(core_speed, fast_mode)
+                .and_then(|_| { 
+                    self.initialized = true;
+                    Ok(())
+                })
+        } else {
+            Ok(())
+        }
+    }
+
+    /// Scan for I²C devices currently connected to the I²C bus. The scan will just try to get an acknowledge message
+    /// from any slave address between 0x00 and 0x7F. If a device is connected this call succeeds and the corresponding
+    /// address is written to the console. This function is typically only used to check for device addresses when a new
+    /// device is connected but it's address is not properly documented.
+    pub fn scan(&self) {
+        let devices = interface::scan_devices();
+        for dev in devices {
+            info!("device detected at {:2X}", dev);
+        };
+    }
+
+    /// Get a new I²C device with the given address. This functions checks whether ther is really a device connected
+    /// with the given address to the I²C bus.
+    /// TODO: ensure there will not be more then 1 request for a device on the same address
+    pub fn get_device(&self, addr: u8) -> I2cResult<I2cDevice> {
+        interface::check_device(addr)
+            .map(|_| {
+                I2cDevice::new(addr as u32)
+            })
+    }
+
+    /// Read a u8 from a device register
+    pub fn read_register_u8(&self, device: I2cDevice, reg: u8) -> I2cResult<u8> {
+        interface::read_reg_u8(device.addr, reg as u32)
+    }
+
+    /// Read a u16 from a device register
+    pub fn read_register_u16(&self, device: I2cDevice, reg: u8) -> I2cResult<u16> {
+        interface::read_reg_u16(device.addr, reg as u32)
+    }
+
+    /// Read a u8 array from a device register
+    pub fn read_register_buff(&self, device: I2cDevice, reg: u8, len: u16) -> I2cResult<Vec<u8>> {
+        interface::read_reg_data(device.addr, reg as u32, len)
+    }
+
+    /// Read a specific bit from a device register. Returns Ok(true) if the bit is set
+    /// or Ok(false) if not.
+    pub fn read_register_bit(&self, device: I2cDevice, reg: u8, offset: u16) -> I2cResult<bool> {
+        interface::read_reg_bits(device.addr, reg as u32, offset, 1)
+            .map(|v| if v == 1 { true } else { false })
+    }
+
+    /// Read specific bits from a device register. The bits in the result are shifted to the right
+    /// so they appear as 0 offset.
+    pub fn read_register_bits(&self, device: I2cDevice, reg: u8, offset: u16, bits: u16) -> I2cResult<u8> {
+        interface::read_reg_bits(device.addr, reg as u32, offset, bits)
+    }
+
+    /// Write u8 data to a device without specifying a register
+    pub fn write_u8(&self, device: I2cDevice, data: u8) -> I2cResult<()> {
+        interface::write_raw_u8(device.addr, data)
+    }
+
+    /// Write u8 data to a device register
+    pub fn write_register_u8(&self, device: I2cDevice, reg: u8, data: u8) -> I2cResult<()> {
+        interface::write_reg_u8(device.addr, reg as u32, data)
+    }
+
+    /// Write u16 data to a device register
+    pub fn write_register_u16(&self, device: I2cDevice, reg: u8, data: u16) -> I2cResult<()> {
+        interface::write_reg_u16(device.addr, reg as u32, data)
+    }
+
+    /// Write a u8 array to a device register
+    pub fn write_register_buff(&self, device: I2cDevice, reg: u8, data: &[u8]) -> I2cResult<()> {
+        interface::write_reg_data(device.addr, reg as u32, data)
+    }
+
+    /// Write a specific bit to a device register. If set is true the bit will be set to 1
+    pub fn write_register_bit(&self, device: I2cDevice, reg: u8, offset: u16, set: bool) -> I2cResult<()> {
+        interface::write_reg_bits(device.addr, reg as u32, offset, 1, set as u8)
+    }
+
+    /// Read specific bits from a device register. The bits in the result are shifted to the right
+    /// so they appear as 0 offset.
+    pub fn write_register_bits(&self, device: I2cDevice, reg: u8, offset: u16, bits: u16, value: u8) -> I2cResult<()> {
+        interface::write_reg_bits(device.addr, reg as u32, offset, bits, value)
+    }
+}
+
+impl I2cDevice {
+    /// create a new I²C deivce instance. This instantiation is only available inside this crate and will be created
+    /// from the I²C implementation only to verify there is really a device connected to this address.
+    pub(crate) fn new(addr: u32) -> Self {
+        I2cDevice {
+            addr: addr,
         }
     }
 }
